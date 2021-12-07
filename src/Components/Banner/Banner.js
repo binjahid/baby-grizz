40f8591edb46f91fa7d5b82d7aadd1730ffe93b8
Muhammad jahid <jahid78784@gmail.com>
2021-12-07 15:40:49 +0600

    header completed

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -3,12 +3,13 @@ import { Box } from "@mui/system";
 import React from "react";
 import banner from "../../Images/banner.png";
 import "./Banner.css";
+import banner2 from "../../Images/banner2.png";
 const Banner = () => {
   return (
     <Box
       sx={{
         marginTop: "1.5rem",
-        paddingBottom: { lg: "5rem", md: "5rem", sm: "7rem" },
+        paddingBottom: { lg: "5rem", md: "5rem", sm: "7rem", xs: "12rem" },
       }}
     >
       <Container fixed>
@@ -22,7 +23,7 @@ const Banner = () => {
             xl={6}
             sx={{ display: "flex", justifyContent: "center" }}
           >
-            <Box>
+            <Box sx={{ position: "relative" }}>
               <Box
                 sx={{
                   fontFamily: "Nunito",
@@ -33,8 +34,15 @@ const Banner = () => {
                     sm: "1.5rem",
                     xs: "2.4rem",
                   },
-                  width: "80%",
+                  width: { md: "80%", lg: "80%", sm: "80%", xs: "100%" },
                   color: "#251400",
+                  textAlign: {
+                    xs: "center",
+                    sm: "left",
+                    md: "left",
+                    lg: "left",
+                    xl: "left",
+                  },
                 }}
               >
                 Baby Grizz <br /> Rewards USDT with <br /> 1000 unique <br />{" "}
@@ -42,20 +50,45 @@ const Banner = () => {
               </Box>
               <Box
                 sx={{
-                  width: "70%",
+                  width: { md: "70%", lg: "70%", sm: "70%", xs: "100%" },
                   color: "#4A4A4A",
                   fontSize: { sm: ".8rem", md: "1rem", lg: "1rem" },
+                  textAlign: {
+                    xs: "center",
+                    sm: "left",
+                    md: "left",
+                    lg: "left",
+                    xl: "left",
+                  },
                 }}
               >
                 Because crypto doesnt have to be ugly. Lets go to the moon in
                 style!
               </Box>
               {/* banner buy button here */}
-              <Box>
+              <Box
+                sx={{
+                  textAlign: {
+                    xs: "center",
+                    sm: "left",
+                    md: "left",
+                    lg: "left",
+                    xl: "left",
+                  },
+                }}
+              >
                 <Link style={{ textDecoration: "none" }} to="#">
                   <button className="banner-buy-btn">Buy on pancakeswap</button>
                 </Link>
               </Box>
+              <div className="sercle-3"></div>
+              <div className="miniSercle3"></div>
+              <div className="miniSercle4">
+                <div className="miniSercle5"></div>
+              </div>
+              <div className="bannerImage2">
+                <img className="banner2" src={banner2} alt="banner" />
+              </div>
             </Box>
           </Grid>
           <Grid
@@ -67,8 +100,17 @@ const Banner = () => {
             xl={6}
             sx={{ display: "flex", justifyContent: "center" }}
           >
-            <Box className="bannerImageContainer">
-              <img className="bannerImg" src={banner} alt="banner" />
+            <Box
+              className="bannerImageContainer"
+              sx={{ mt: { lg: 0, md: 0, sm: 0, xs: "1rem" } }}
+            >
+              <div className="img">
+                <img className="bannerImg" src={banner} alt="banner" />
+              </div>
+              <div className="sercle-1"></div>
+              <div className="sercle-2"></div>
+              <div className="miniSercle1"></div>
+              <div className="miniSercle2"></div>
             </Box>
           </Grid>
         </Grid>
