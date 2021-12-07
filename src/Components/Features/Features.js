439116a8c9bbc613ffda44baf93c24be6678da42
Muhammad jahid <jahid78784@gmail.com>
2021-12-07 18:28:30 +0600

    features done

---
+++ src/Components/Features/Features.js
@@ -0,0 +1,131 @@
+import { Container, Grid, Paper } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import "./Features.css";
+import featureIcon1 from "../../Images/fetureIcon1.png";
+import featureIcon2 from "../../Images/featureIcon2.png";
+import featureIcon3 from "../../Images/featureIcon3.png";
+import featureImage from "../../Images/featureImage.png";
+const Features = () => {
+  return (
+    <div>
+      <Container>
+        <Box sx={{ position: "relative" }}>
+          <p className="features-title">Some of Our Features</p>
+          <div className="titleCercle"></div>
+        </Box>
+        <Grid container spacing={1}>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={6}
+            lg={6}
+            xl={6}
+            sx={{ position: "relative" }}
+          >
+            {/* Passive Income */}
+            <Paper elevation={2} sx={{ p: 1, borderRadius: "10px", mb: 2.5 }}>
+              <Box sx={{ display: "flex" }}>
+                <div className="feature-icon-container">
+                  <img
+                    className="feature-icon"
+                    src={featureIcon1}
+                    alt="featureIcon1"
+                  />
+                </div>
+                <div className="feature-description-container">
+                  <p className="featureTitle">Passive Income</p>
+                  <p className="featureDescription">
+                    6% of every transaction goes directly from BNB to USDT and
+                    is distributed to our holders. That way you don’t impact the
+                    charts just by collecting your rewards.
+                  </p>
+                </div>
+              </Box>
+            </Paper>
+            {/* NFT */}
+            <Paper
+              elevation={2}
+              sx={{
+                position: "relative",
+                p: 1,
+                borderRadius: "10px",
+                backgroundImage: "linear-gradient(to right, #fe6806, #ff9900)",
+                mb: 2.5,
+                left: "65px",
+              }}
+            >
+              <Box sx={{ display: "flex" }}>
+                <div className="feature-icon-container">
+                  <img
+                    className="feature-icon-NFT"
+                    src={featureIcon2}
+                    alt="featureIcon1"
+                  />
+                </div>
+                <div className="feature-description-container-NFT">
+                  <p className="featureTitle">NFT</p>
+                  <p className="featureDescription">
+                    Unique and lovable NFTs made for the BabyGrizz community.
+                    Limited number with rare characteristics. These were built
+                    to be featured and will brighten your day. Taking the ugly
+                    out of crypto.
+                  </p>
+                </div>
+              </Box>
+            </Paper>
+            {/* Zero Risk Factors */}
+            <Paper elevation={2} sx={{ p: 1, borderRadius: "10px" }}>
+              <Box sx={{ display: "flex" }}>
+                <div className="feature-icon-container">
+                  <img
+                    className="feature-icon"
+                    src={featureIcon3}
+                    alt="featureIcon1"
+                  />
+                </div>
+                <div className="feature-description-container">
+                  <p className="featureTitle">Zero Risk Factors</p>
+                  <p className="featureDescription">
+                    Audited by Tech Audit and KYC’D Dev Team here to push this
+                    into a 1000x moonshot. Built for the community this is as
+                    safe as it gets.
+                  </p>
+                </div>
+              </Box>
+            </Paper>
+            <div className="descriptionSmallCercle1"></div>
+            <div className="descriptionSmallCercle2"></div>
+          </Grid>
+          {/* Feature Image Goes Here */}
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={6}
+            lg={6}
+            xl={6}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              position: "relative",
+            }}
+          >
+            <div className="featureImageContianer">
+              <img className="featureImage" src={featureImage} alt="feature" />
+              <div className="featureCercle1"></div>
+              <div className="featureCercle2"></div>
+              <div className="miniCercle1"></div>
+              <div className="miniCercle2"></div>
+              <div className="miniCercle3"></div>
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
+
+export default Features;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Features from "../Features/Features";
 import Header from "../Header/Header";
 
 const Home = () => {
   return (
     <div>
       <Header></Header>
+      <Features></Features>
     </div>
   );
 };
