4ab3e5f5d5b38adfc6c4e6978706e548dc566bd8
Muhammad jahid <jahid78784@gmail.com>
2021-12-08 01:29:13 +0600

    journey section completed

--- src/Components/OurJourney/OurJourney.js
+++ src/Components/OurJourney/OurJourney.js
@@ -1,19 +1,73 @@
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, Paper } from "@mui/material";
 import React from "react";
 import journeyImg from "../../Images/journeyImg.png";
-import journeyBackgroud from "../../Images/journeyBackground.png";
 import journeyArrow from "../../Images/journeyArrow.png";
 import "./OurJourney.css";
+import journeySmallImg from "../../Images/journeySmallImg.png";
 const OurJourney = () => {
+  // Edit Data here to update Jounrney timeline
+  const jourNeyData = {
+    questionOne: {
+      name: "December 2021",
+      description:
+        "Presale - Marketing - Launch We will build the strongest foundation for this token so it can withstand the test of time.",
+    },
+    questionTwo: {
+      name: "Q1 2022",
+      description:
+        "NFT Marketplace and NFT Partnerships Because NFT’s are awesome!",
+    },
+    questionThree: {
+      name: "Q2 2022",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae adipiscing.",
+    },
+    questionFour: {
+      name: "Q3 2022",
+      description:
+        "Purchase Tesla Company from Elon Musk for $1,000,000,000,000",
+    },
+  };
   return (
     <div className="jounrneyContainer">
+      <div className="ourJourneyTitleContainer">
+        <h2 className="ourJourneyTitle">Our Journey</h2>
+        <p className="ourJourneyDescription">
+          Having been investors in this space for a while, we wanted to take all
+          of our favorite pieces of crypto and put them together in one safe
+          project. Building something that is fun while also being financially
+          impactful. We want to set an example for others to follow, so we have.
+          Welcome to Baby Grizz.
+        </p>
+        <div className="journeyCercle2"></div>
+        <div>
+          <img
+            className="journeySmallImg"
+            src={journeySmallImg}
+            alt="journeySmallImg"
+          />
+        </div>
+        <div className="jouneyCercle3"></div>
+        <div className="jouneyCercle4"></div>
+        <div className="jouneyCercle5"></div>
+      </div>
       <Container fixed>
         <Grid container spacing={2}>
-          <Grid item xs={12} sm={12} md={3} lg={3} xl={3}>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={3}
+            lg={3}
+            xl={3}
+            sx={{ position: "relative" }}
+          >
             <div className="journey-img-container">
               <img className="journeyImg" src={journeyImg} alt="journeyImg" />
+              <div className="journeyCercle"></div>
             </div>
           </Grid>
+          {/* Time line Container */}
           <Grid
             item
             xs={12}
@@ -28,6 +82,44 @@ const OurJourney = () => {
             }}
           >
             <div className="journeyTimelineContainer">
+              <div className="journeyTimelineLeft">
+                <div className="journeyTimeline1">
+                  <Paper
+                    elevation={1}
+                    sx={{
+                      width: "350px",
+                      p: "5px 20px",
+                      borderRadius: "10px",
+                      backgroundColor: "#FFFEF9",
+                    }}
+                  >
+                    <h2 className="timeLineHeader">
+                      {jourNeyData.questionTwo.name}
+                    </h2>
+                    <p className="timeLineDes">
+                      {jourNeyData.questionTwo.description}
+                    </p>
+                  </Paper>
+                </div>
+                <div className="journeyTimeline2">
+                  <Paper
+                    elevation={1}
+                    sx={{
+                      width: "350px",
+                      p: "5px 20px",
+                      borderRadius: "10px",
+                      backgroundColor: "#FFFEF9",
+                    }}
+                  >
+                    <h2 className="timeLineHeader">
+                      {jourNeyData.questionFour.name}
+                    </h2>
+                    <p className="timeLineDes">
+                      {jourNeyData.questionFour.description}
+                    </p>
+                  </Paper>
+                </div>
+              </div>
               <div className="journeyArrow">
                 <img
                   className="journeyArrowImg"
@@ -45,6 +137,44 @@ const OurJourney = () => {
                 </div>
                 <div className="journeyDot4">
                   <div className="journeySamllDot4"></div>
+                </div>
+              </div>
+              <div className="journeyTimelineRight">
+                <div className="journeyTimeline3">
+                  <Paper
+                    elevation={1}
+                    sx={{
+                      width: "350px",
+                      p: "5px 20px",
+                      borderRadius: "10px",
+                      backgroundColor: "#FFFEF9",
+                    }}
+                  >
+                    <h2 className="timeLineHeader">
+                      {jourNeyData.questionOne.name}
+                    </h2>
+                    <p className="timeLineDes">
+                      {jourNeyData.questionOne.description}
+                    </p>
+                  </Paper>
+                </div>
+                <div className="journeyTimeline4">
+                  <Paper
+                    elevation={1}
+                    sx={{
+                      width: "350px",
+                      p: "5px 20px",
+                      borderRadius: "10px",
+                      backgroundColor: "#FFFEF9",
+                    }}
+                  >
+                    <h2 className="timeLineHeader">
+                      {jourNeyData.questionThree.name}
+                    </h2>
+                    <p className="timeLineDes">
+                      {jourNeyData.questionThree.description}
+                    </p>
+                  </Paper>
                 </div>
               </div>
             </div>
